2d8013dda12071771e336461751999c733d4f7e5
Dane Smith <doctorhowser@gmail.com>
2020-08-28 10:43:22 -0500

    add movie route added

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const movieRouter = require('./routes/movie.router.js')
 const port = process.env.PORT || 5000;
 
 /** ---------- MIDDLEWARE ---------- **/
@@ -8,7 +9,7 @@ app.use(bodyParser.json()); // needed for angular requests
 app.use(express.static('build'));
 
 /** ---------- ROUTES ---------- **/
-
+app.use('/api/movie', movieRouter)
 
 /** ---------- START SERVER ---------- **/
 app.listen(port, function () {
