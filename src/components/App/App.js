b174247d2949a5333fb3a48841dc8de2c742ee5c
Edan Schwartz <edanschwartz@gmail.com>
2021-01-15 14:29:04 -0600

    Update to use functional components

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,20 +1,17 @@
-import React, { Component } from 'react';
 import {HashRouter as Router, Route} from 'react-router-dom';
 import './App.css';
 
-class App extends Component {
-  // Renders the entire app on the DOM
-  render() {
-    return (
-      <div className="App">
-        <h1>Movies!</h1>
-        <Router>
-          {/* ADD PAGES! */}
-        </Router>
-        <p>Empty Page</p>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <h1>Movies!</h1>
+      <Router>
+        {/* ADD PAGES! */}
+      </Router>
+      <p>Empty Page</p>
+    </div>
+  );
 }
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App.js';
-import registerServiceWorker from './registerServiceWorker';
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 // Provider allows us to use redux within our react app
 import { Provider } from 'react-redux';
@@ -51,6 +50,11 @@ const storeInstance = createStore(
 // Pass rootSaga into our sagaMiddleware
 sagaMiddleware.run(rootSaga);
 
-ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, 
-    document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+    <React.StrictMode>
+        <Provider store={storeInstance}>
+        <App />
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
+);
