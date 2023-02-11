2efcae747c1695988abafa5853e9894d8b98e548
Kathryn Szombatfalvy <kathrynszombatfalvy@gmail.com>
2023-02-11 10:16:17 -0600

    added comments to details page

--- src/components/App/Details.jsx
+++ src/components/App/Details.jsx
@@ -3,47 +3,48 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useHistory, useParams } from "react-router-dom";
 
+function Details() {
+  const dispatch = useDispatch();
 
+  //this pulls in the movie id from the url
+  let { id } = useParams();
+  id = Number(id);
 
+  const history = useHistory();
 
+  //pull in all movies from store
+  const allMovies = useSelector((store) => store.movies);
+  //filter through movies to find the one with matching id
+  let movie = allMovies.find((item) => item.id === id);
 
-function Details (){
-    const dispatch = useDispatch();
+  //pull in all genres from store
+  const genres = useSelector((store) => store.genres);
+  //filter through genres to find all the genres related to this movie id
+  const genreList = genres.filter((item) => item.movie_id === id);
+  //console.log("this is the list of genres for this movie", genreList);
 
-    let {id}  = useParams();
-    id = Number(id);
+  //on initial load, tell saga to get a genres from DB
+  useEffect(() => {
+    dispatch({ type: "FETCH_GENRES" });
+  }, []);
 
-    const history = useHistory();
-
-    const allMovies = useSelector(store => store.movies);
-    let movie = allMovies.find(item => item.id === id)
-
-    const genres = useSelector(store => store.genres);
-    const genreList = genres.filter(item => item.movie_id === id);
-    console.log("this is the list of genres for this movie", genreList);
-
-    useEffect(() =>{
-        dispatch({ type: 'FETCH_GENRES' });
-    },[])
-    
-    
-
-    
-
-    return (<>
-   
-    <div>
+  return (
+    <>
+      <div>
         <h2>{movie.title}</h2>
         <img src={movie.poster} />
         <h3>Genres:</h3>
         <ul>
-            {genreList.map((item) => <li>{item.name}</li>)}
+          {genreList.map((item) => (
+            <li>{item.name}</li>
+          ))}
         </ul>
         <h4>{movie.description}</h4>
-        
-    </div>
+      </div>
 
-    <button onClick={() => history.push('/')}>back to movies list</button></>);
+      <button onClick={() => history.push("/")}>back to movies list</button>
+    </>
+  );
 }
 
 export default Details;
