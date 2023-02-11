0e710661ab8f01dae6b0b63f7e7900b6f3bff0e1
Kathryn Szombatfalvy <kathrynszombatfalvy@gmail.com>
2023-02-11 10:20:59 -0600

    added comments to genre.router.js

--- server/routes/genre.router.js
+++ server/routes/genre.router.js
@@ -2,13 +2,14 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../modules/pool')
 
+//Select a list from the DB that includes all the movies and their different assigned genres. Sends back to saga.
 router.get('/', (req, res) => {
-  console.log("i am inside the router.get");
+  //console.log("i am inside the router.get");
   const query = `SELECT "movie_id", "title", "name" FROM "movies_genres"
   JOIN "movies" ON "movies"."id" = "movies_genres"."movie_id"
   JOIN "genres" ON "genres"."id" = "movies_genres"."genre_id";`;
   pool.query(query)
-    .then( result => {
+    .then(result => {
       res.send(result.rows);
     })
     .catch(err => {
